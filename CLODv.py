908116195ffd7f00feba75992e40c061f59f2c80
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2015-02-06 11:18:16 +0100

    Summing total freqs

--- CLODv.py
+++ CLODv.py
@@ -6,6 +6,8 @@
 import rdflib
 import os
 import logging
+import csv
+from itertools import combinations
 from ConfigParser import SafeConfigParser
 
 CONFIG_INI = "config.ini"
@@ -15,6 +17,35 @@ class CLODv:
         self.log = logging.getLogger("CLODv")
         self.config = __config
         self.PATH = self.config.get('general', 'path')
+        
+        self.parseFiles()
+
+    def parseFiles(self):
+        '''
+        Parses all combinations of files in the stats dir path
+        '''
+        for root, dirs, files in os.walk(self.PATH):
+            for f in combinations(files, 2):
+                self.parsePair(f)
+
+    def parsePair(self, p):
+        '''
+        Parses a pair of files in the stats dir
+        '''
+        currFileA = list(csv.reader(open(p[0], 'rb'), delimiter='\t'))
+        currFileB = list(csv.reader(open(p[1], 'rb'), delimiter='\t'))
+        currTotalA = self.sumTotal(currFileA)
+        currTotalB = self.sumTotal(currFileB)
+
+    def sumTotal(self, f):
+        '''
+        Sums the total freqs of stats file f
+        '''
+        total = 0
+        for line in f:
+            total += line[1]
+        self.log.info("File %s sums total %s" % (f, total))
+
         
 if __name__ == "__main__":
     # Config
